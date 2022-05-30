3ea8b8c4f8ce9230d424bda5c7cc1f82b6a5b117
Jeroen Janssens <jeroen.janssens@accentjobs.be>
2022-05-30 21:13:17 +0200

    add snackservice to distributor contract

--- frontend/hydroblox-app/src/app/shared/services/distributorcontract.service.ts
+++ frontend/hydroblox-app/src/app/shared/services/distributorcontract.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ErrorCodes, ErrorSnackService } from './errorsnack.service';
 import { LoaderService } from './loader.service';
 import { Web3Service } from './web3.service';
 
@@ -9,18 +10,44 @@ export class DistributorContractService {
 
     constructor(
       private web3Service: Web3Service,
-      private loadService: LoaderService) {
+      private loadService: LoaderService,
+      private snackService: ErrorSnackService) {
     }
 
     async isOwner(): Promise<boolean> {
+      var result: boolean = false;
+      try {
+        this.loadService.show();
+
         var contract = this.web3Service.getDistributorContract();
-        var result = await contract.methods.isOwner().call();
-        return result;
+        result = await contract.methods.isOwner().call();
+      }
+      catch (error) {
+        console.error(error);
+        this.snackService.showError(ErrorCodes.Unexpected);
+      }
+      finally {
+        this.loadService.hide();
+      }
+
+      return result;
     }
 
     async currentState(): Promise<string> {
-      var contract = this.web3Service.getDistributorContract();
-      var result: string = await contract.methods.state().call();
+      var result: string | undefined;
+      try {
+        this.loadService.show();
+
+        var contract = this.web3Service.getDistributorContract();
+        result = await contract.methods.state().call();
+      }
+      catch (error) {
+        console.error(error);
+        this.snackService.showError(ErrorCodes.Unexpected);
+      }
+      finally {
+        this.loadService.hide();
+      }
 
       if (result === '0') {
         return 'Enrollment';
@@ -37,10 +64,15 @@ export class DistributorContractService {
     }
 
     async transitionToNextState(): Promise<void> {
-      this.loadService.show();
       try {
+        this.loadService.show();
+
         var contract = this.web3Service.getDistributorContract();
         await contract.methods.transitionToNextState().send();
+      }
+      catch (error) {
+        console.error(error);
+        this.snackService.showError(ErrorCodes.Unexpected);
       }
       finally {
         this.loadService.hide();
