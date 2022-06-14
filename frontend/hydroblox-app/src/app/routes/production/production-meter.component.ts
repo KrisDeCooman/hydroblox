efea5f6b78ac67d610782df96d771f3faa91b98f
Jeroen Janssens <jeroen.janssens@accentjobs.be>
2022-06-14 17:00:38 +0200

    fix naming conflict

--- frontend/hydroblox-app/src/app/routes/production/production-meter.component.ts
+++ frontend/hydroblox-app/src/app/routes/production/production-meter.component.ts
@@ -9,33 +9,33 @@ import { StorageContractService } from 'src/app/shared/contracts/storagecontract
 
 export class ProductionMeterComponent implements OnInit {
   productionWater!: number;
-    currentState: string = '';
-    isSubscribedProducer: boolean = false;
-    isNotsubscribedProducer: boolean = true;
+  currentState: string = '';
+  isSubscribedProducer: boolean = false;
+  isNotsubscribedProducer: boolean = true;
 
 
-    
-  
-    constructor(private distributorContractService: DistributorContractService,
-      private storageContractService: StorageContractService) { }
-  
-    async ngOnInit(): Promise<void> {
-      this.currentState = await this.distributorContractService.currentState();
-      this.isSubscribedProducer = await this.storageContractService.isSubscribedProducer();
-      this.isNotsubscribedProducer =! await this.storageContractService.isSubscribedProducer();
-    }
-    async subscribe() {
-        await this.distributorContractService.subscribeProducer();
-      
-  
-    }
-    async producewater( productionWater:number) {
-      await this.distributorContractService.produce(productionWater);
-    }
-    async claimProducer() {
-      await this.distributorContractService.claimTokensAsProducer();
-    }
-    
+
+
+  constructor(private distributorContractService: DistributorContractService,
+    private storageContractService: StorageContractService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.currentState = await this.distributorContractService.currentState();
+    this.isSubscribedProducer = await this.storageContractService.isSubscribedProducer();
+    this.isNotsubscribedProducer = ! await this.storageContractService.isSubscribedProducer();
   }
-  
-  
+  async subscribe() {
+    await this.distributorContractService.subscribeAsProducer();
+
+
+  }
+  async producewater(productionWater: number) {
+    await this.distributorContractService.produce(productionWater);
+  }
+  async claimProducer() {
+    await this.distributorContractService.claimTokensAsProducer();
+  }
+
+}
+
+
